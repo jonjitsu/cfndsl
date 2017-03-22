423ff7588c734fbc61c58f077ad71e6e8ef3b134
Jonathan Willis <jonathanallenwillis@gmail.com>
2017-03-21 21:06:06 -0400

    Added types.yaml sorter in order to be able to merge old + new

--- lib/cfndsl/aws/generator.rb
+++ lib/cfndsl/aws/generator.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'yaml'
 
 def resources(spec)
   spec['ResourceTypes'] || spec[:ResourceTypes]
@@ -6,6 +7,15 @@ end
 
 def properties(resource)
   resource['Properties'] || resource[:Properties]
+end
+
+# For compatibility with cfndsl types
+def transform_type(type)
+  if type.is_a?(String) && type.downcase == 'json'
+    'JSON'
+  else
+    type
+  end
 end
 
 def property_type(prop)
@@ -28,7 +38,7 @@ end
 
 def property_types(resource)
   properties(resource).reduce({}) do |c, (name, info)|
-    c[name] = property_type(info)
+    c[name] = transform_type(property_type(info))
     c
   end
 end
@@ -44,45 +54,6 @@ def ordered(hash, start_hash={})
   hash.keys.sort.reduce(start_hash) do |c, key|
     c[key] = hash[key]
     c
-  end
-end
-
-def add_type(types, prop, prefix='')
-  if prop.key? 'PrimitiveType'
-    val = prop['PrimitiveType']
-    types[:primitive].add val
-  elsif prop.key? 'PrimitiveItemType'
-    val = prop['PrimitiveItemType']
-    types[:primitive].add val
-  elsif prop.key? 'ItemType'
-    val = prop['ItemType']
-    p prefix, val
-    types[:other].add(prefix + val)
-  elsif prop.key? 'Type' and prop['Type'].downcase != 'list'
-    val = prop['Type']
-    types[:other].add(prefix + val)
-  end
-  types
-end
-
-def primitives(collected_types)
-  raise 'no primitives' unless collected_types.key? :primitive
-  collected_types[:primitive].reduce({}) do |c, type|
-    c[type] = type
-    c
-  end
-end
-
-def add_types(types, resource, resource_name)
-  properties(resource).each do |name, info|
-    add_type(types, info, resource_name)
-  end
-  types
-end
-
-def collect_types(spec)
-  resources(spec).reduce({primitive: Set.new, other: Set.new}) do |types, (name, info)|
-    add_types(types, info, name + '.')
   end
 end
 
@@ -114,6 +85,7 @@ end
 
 def primitives_to_hash(primitives)
   primitives.reduce({}) do |c, type|
+    type = transform_type(type)
     c[type] = type
     c
   end
@@ -141,8 +113,16 @@ def generate(spec)
     'Types' => ordered(generate_types(spec)) }
 end
 
+def sort_types_file(filename)
+  data = load_spec_file(filename)
+  {
+    'Resources' => ordered(data['Resources']),
+    'Types' => ordered(data['Types'])
+  }
+end
+
 def load_spec_file(filename)
-  JSON.parse(File.read(filename))
+  YAML.safe_load(File.read(filename))
 end
 
 def from_file(filename)
